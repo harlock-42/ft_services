9b2d31c9e100c3d193a47e042bd2c3003046196d
tallaire42 <allaire.terry.at@gmail.com>
2021-01-19 16:17:05 +0100

    matallb configuring

---
+++ setup.sh
@@ -0,0 +1,42 @@
+# initialization of minikube
+
+#minikube delete
+
+# switch on minikube
+
+minikube start --vm-driver=docker
+
+# modification of kube-proxy
+
+# check if the modification is possible
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+# apply the modification
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+# install metallb namespace
+
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/namespace.yaml
+
+# install metallb
+
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/metallb.yaml
+
+# create and configure metallb secret
+
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# switch namespace to kube-system
+
+kubectl config set-context --current --namespace=metallb-system
+
+
+# apply metallb configMap
+
+kubectl apply -f metallb-configMap
