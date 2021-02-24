98439d78a2ffe13a3bbf20eb256b1c52c4d2ea00
tallaire42 <allaire.terry.at@gmail.com>
2021-02-24 16:56:00 +0100

    we are hot and restart ft_services from 0

---
+++ README_docker.md
@@ -0,0 +1,13 @@
+## dispay images
+```
+docker images
+```
+
+# Launch a contener
+
+exemple :
+```
+docker run -p 8000:80 --name CONTENER_NAME -d IMAGE_NAME .
+```
+
+`-p 8000:80` is to redirect contener's port to the host machin
--- setup.sh
+++ setup.sh
@@ -1,49 +1,11 @@
-# set minikube config
-
-minikube config set vm-driver docker
-
-# initialization of minikube
+# clean the cluster
 
 minikube delete
 
-# switch on minikube
+# Start host machin 
 
-minikube start
+service docker start
 
-# modification of kube-proxy
+# start your cluster
 
-# check if the modification is possible
-
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl diff -f - -n kube-system
-
-# apply the modification
-
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
-
-# install metallb namespace
-
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/namespace.yaml
-
-# install metallb
-
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.5/manifests/metallb.yaml
-
-# create and configure metallb secret
-
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-# switch namespace to kube-system
-
-kubectl config set-context --current --namespace=metallb-system
-
-# apply metallb configMap
-
-kubectl apply -f metallb-configMap.yaml
-
-# dashboard's deployment
-
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
+minikube start --vm-driver=docker
