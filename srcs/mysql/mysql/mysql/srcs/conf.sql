1c55dc6cec6c7dd79ab07a77244e6e7ae6935045
tallaire42 <allaire.terry.at@gmail.com>
2021-03-06 09:34:21 +0100

    cleaning

--- srcs/mysql/mysql/mysql/srcs/conf.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE wordpress;
-
-CREATE USER 'thsembel' IDENTIFIED BY 'thsembel';
-GRANT USAGE ON wordpress.* TO 'thsembel'@'localhost' IDENTIFIED BY 'thsembel';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'thsembel'@'localhost';
-FLUSH PRIVILEGES;
