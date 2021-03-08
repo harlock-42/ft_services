7446b81d6fa3b27911c173c7dc2b076efa6a1d01
tallaire42 <allaire.terry.at@gmail.com>
2021-03-08 17:02:44 +0100

    wp, php, mysql and nginx deployment done

--- setup.sh
+++ setup.sh
@@ -1,14 +1,43 @@
+install_metallb()
+{
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+}
 
-service nginx stop
+# Start host machin
+
+sudo service docker start
+
+minikube delete
+
+sudo service nginx stop
+
+sudo service mariadb stop
 
 # clean the cluster
 
 minikube delete
 
-# Start host machin 
-
-service docker start
-
 # start your cluster
 
 minikube start --vm-driver=docker
+
+eval $(minikube docker-env)
+
+install_metallb
+
+# Building images
+
+docker build -t nginx-img ./srcs/nginx/.
+docker build -t mysql-img ./srcs/mysql/.
+docker build -t php-img ./srcs/phpmyadmin/.
+docker build -t wp-img ./srcs/wordpress/.
+
+# sart deployments
+
+kubectl apply -f ./srcs/nginx/nginx-depl.yaml
+kubectl apply -f ./srcs/mysql/mysql-depl.yaml
+kubectl apply -f ./srcs/phpmyadmin/php-depl.yaml
+kubectl apply -f ./srcs/wordpress/wp-depl.yaml
+
