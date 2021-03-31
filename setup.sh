a484af59b88b081ae5961fb7511ae0a74c7dac11
tallaire42 <allaire.terry.at@gmail.com>
2021-03-31 13:47:45 +0200

    setup is pretty

--- setup.sh
+++ setup.sh
@@ -14,56 +14,111 @@ NC='\e[0m'
 #### FT_SERVICES ####
 #####################
 
+echo "${RED}"
+echo "##################################################################"
+echo "#                                                                #"
+echo "# ***** *****          ***  ***** ****  *   * *****   **  *****  #"
+echo "# *       *           *     *     *   * *   *   *    *  * *      #"
+echo "# ***     *             *   ***   ****   * *    *   *     ***    #"
+echo "# *       *               * *     * *    * *    *    *  * *      #"
+echo "# *       *    *****   ***  ***** *   *   *   *****   **  *****  #"
+echo "#                                                                #"
+echo "##################################################################"
+echo "${NC}"
+
+
 apply_yaml()
 {
 	# apply metallb
-
-	kubectl apply -f ./srcs/metallb.yaml
+	
+	echo "${ORANGE}Applying metallb${NC}"
+	kubectl apply -f ./srcs/metallb.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
 
 	# apply volume
 
-	kubectl apply -f ./srcs/influxdb/influxdb-volume.yaml
-	kubectl apply -f ./srcs/mysql/mysql-volume.yaml
+	echo "${ORANGE}Applying volumes${NC}"
+
+	echo "${ORANGE}influxdb volumes..${NC}"
+	kubectl apply -f ./srcs/influxdb/influxdb-volume.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}mysql volumes..${NC}"
+	kubectl apply -f ./srcs/mysql/mysql-volume.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
 
 	# start databases
 
-	kubectl apply -f ./srcs/mysql/mysql.yaml
-	kubectl apply -f ./srcs/influxdb/influxdb.yaml
+	echo "${ORANGE}Applying databases${NC}"
+
+	echo "${ORANGE}mysql..${NC}"
+	kubectl apply -f ./srcs/mysql/mysql.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}influxdb..${NC}"
+	kubectl apply -f ./srcs/influxdb/influxdb.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
 
 
 	sleep 5
 	# start deployments
 
-	kubectl apply -f ./srcs/grafana/grafana.yaml
-	kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
-	kubectl apply -f ./srcs/wordpress/wordpress.yaml
-	kubectl apply -f ./srcs/ftps/ftps.yaml
-	kubectl apply -f ./srcs/nginx/nginx.yaml
-	kubectl apply -f ./srcs/telegraf/telegraf.yaml
+	echo "${ORANGE}Applying deployments${NC}"
+	
+	echo "${ORANGE}grafana..${NC}"
+	kubectl apply -f ./srcs/grafana/grafana.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}phpmyadmin..${NC}"
+	kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}wordpress..${NC}"
+	kubectl apply -f ./srcs/wordpress/wordpress.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}ftps..${NC}"
+	kubectl apply -f ./srcs/ftps/ftps.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}nginx..${NC}"
+	kubectl apply -f ./srcs/nginx/nginx.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
+	echo "${ORANGE}telegraf..${NC}"
+	kubectl apply -f ./srcs/telegraf/telegraf.yaml > ./srcs/log/logs
+	echo "${GREEN}Done !${NC}"
 }
 
 build_image()
 {
-	docker build -t wp-img ./srcs/wordpress/.
-	docker build -t mysql-img ./srcs/mysql/.
-	docker build -t php-img ./srcs/phpmyadmin/.
-	docker build -t nginx-img ./srcs/nginx/.
-	docker build -t ftps-img ./srcs/ftps/.
-	docker build -t influxdb-img ./srcs/influxdb/.
-	docker build -t grafana-img ./srcs/grafana/.
+	echo "${ORANGE}wordpress image building...${NC}"
+	docker build -t wp-img ./srcs/wordpress/. > ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}mysql image building...${NC}"
+	docker build -t mysql-img ./srcs/mysql/. > ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}phpmyadmin image building...${NC}"
+	docker build -t php-img ./srcs/phpmyadmin/. > ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}nginx image building...${NC}"
+	docker build -t nginx-img ./srcs/nginx/. > ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}ftps image building...${NC}"
+	docker build -t ftps-img ./srcs/ftps/. ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}influxdb image building...${NC}"
+	docker build -t influxdb-img ./srcs/influxdb/. ./srcs/log/logs
+	echo "${GREEN}done !\n"
+	echo "${ORANGE}grafana image building...${NC}"
+	docker build -t grafana-img ./srcs/grafana/. ./srcs/log/logs
+	echo "${GREEN}done !\n"
 }
 
 start_minikube()
 {
-	minikube delete
-	minikube start --vm-driver=docker
+	minikube delete > ./srcs/log/logs
+	minikube start --vm-driver=docker > ./srcs/log/logs
 }
 
 install_metallb()
 {
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml > ./srcs/log/logs
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml > ./srcs/log/logs
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" ./srcs/log/logs 2>&1
 }
 
 install_minikube()
@@ -113,17 +168,39 @@ check_installation()
 	fi
 }
 
+rm -rf ./srcs/log > /dev/null
+mkdir -p ./srcs/log > /dev/null
+touch ./srcs/log/logs > /dev/null
+
+echo "${ORANGE}Please give the permission to work (user42): ${NC}"
+sudo ls / > /dev/null
+
+echo "${ORANGE}First step, i'm gonna check your installation${NC}\n"
 check_installation
+echo "${GREEN}Installation checking done !${NC}\n"
 
-#start_minikube
+echo "${ORANGE}Step two, i'm starting minikube${NC}"
+start_minikube
+echo "${GREEN}Done !${NC}"
 
-#install_metallb
 
-# install FileZilla
-#sudo apt-get install filezilla
+echo "${ORANGE}Step three, i'm installing metallb${NC}"
+install_metallb
+echo "${GREEN}Done !${NC}"
 
-#eval $(minikube docker-env)
+echo "${ORANGE}Step four, i'm installing Filezilla${NC}"
+sudo apt-get install filezilla
+echo "${GREEN}Done !${NC}"
 
-#build_image
 
-#apply_yaml
+eval $(minikube docker-env)
+
+echo "${ORANGE}Well, now hard work begins !\nWe are going to build your image. In you interest, i hope you you like staying watching the screen during a while${NC}"
+build_image
+
+echo "${ORANGE}Your images are built, Good ! Last hard step, applying your .yaml files${NC}"
+apply_yaml
+echo "${GREEN}Well Done !${NC}"
+
+echo "${CYAN}We have finished the work. Now i'm gonna launch for you the dashboard. Enjoy !${NC}"
+minikube dashboard
